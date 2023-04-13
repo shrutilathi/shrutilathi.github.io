6c9fc7126f878681b189d441a0f32232244c46e4
Shruti Lathi <lathishruti2002@gmail.com>
2023-04-13 20:03:48 +0530

    Display component added

---
+++ src/component/Display.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useSelector,useDispatch } from 'react-redux';
+import "../App.css"
+const Display = () => {
+ const myState=useSelector((state)=>state.reducer);
+  return (
+    <>
+    <h1>User Details</h1>
+    <div>
+    <table className="Display">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Age</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {myState.map((item) => {
+                  return (
+                    <tr key={item.id}>
+                      <td>{item.name}</td>
+                      <td>{item.email}</td>
+                      <td>{item.age}</td>
+                      <td>
+                        <button
+                        //   onClick={() => {
+                        //     editRecord(item.id);
+                        //   }}
+                          className="btn-edit"
+                        >
+                          Edit
+                        </button>
+                        <button
+                        //   onClick={() => dispatch(deleteUser(item.id))}
+                          className="btn-delete"
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+            </div>
+            </>
+  )
+}
+
+export default Display
